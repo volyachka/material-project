79323834922a3c144ad7965948eef7fc007bdf60
Artem Maevskiy <amaevskij@gmail.com>
2023-11-09 13:59:17 +0800

    slim dataframe; diffusion coefficient plots

--- aiida_utils/load_data.py
+++ aiida_utils/load_data.py
@@ -73,10 +73,32 @@ def load_data(archive_file: str = "../data_Kahle2020/migrated.aiida") -> pd.Data
     print("load_data(): retrieving calc job nodes")
     df["creators"] = find_creator_calc_job_nodes(df["traj"])
 
-    print("load_data(): extracting temperature info")
+    print("load_data(): extracting temperature info from calc jobs")
     df["temp"] = [
         get_temperature([c.pk for c in creators])
         for creators in tqdm(df["creators"], desc="Extracting temperature info")
     ]
 
+    print("load_data(): extracting temperature info from trajectory attributes")
+    temperatures = [float(traj.attributes["temperature_thermostat"]) for traj in df["traj"]]
+    assert (df["temp"] == temperatures).all()
+
+    print("load_data(): extracting diffusion info")
+    df["diff_mean"] = [arr.attributes["Li"]["diffusion_mean_cm2_s"] for arr in df["diff"]]
+    df["diff_std"] = [arr.attributes["Li"]["diffusion_std_cm2_s"] for arr in df["diff"]]
+    df["diff_sem"] = [arr.attributes["Li"]["diffusion_sem_cm2_s"] for arr in df["diff"]]
+
+    print("load_data(): extracting labels and ase structures")
+    df["stru_label"] = [stru.label for stru in df["stru"]]
+    df["stru_ase"] = [stru.get_ase() for stru in df["stru"]]
+
+    print("load_data(): extracting source DB info")
+    df["stru_db"] = [stru.extras.get("source", {}).get("db_name", "NA") for stru in df["stru"]]
+    df["stru_id"] = [stru.extras.get("source", {}).get("id", "NA") for stru in df["stru"]]
+
     return df
+
+@memory.cache
+def load_data_slim(archive_file: str = "../data_Kahle2020/migrated.aiida") -> pd.DataFrame:
+    df = load_data(archive_file=archive_file)
+    return df[["stru_label", "stru_db", "stru_id", "stru_ase", "temp", "diff_mean", "diff_std", "diff_sem"]]
