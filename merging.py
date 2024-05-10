37af5fb482bea2548f9d8a6ae298fe95d3626b36
volyachka <volyachkaa@gmail.com>
2024-05-10 15:36:13 +0300

    add some new evaluation fuctions

--- merging.py
+++ merging.py
@@ -1,10 +1,6 @@
 import os
 import io
-import sys
 import re
-import time
-import math
-import pickle
 
 import numpy as np
 import pandas as pd
@@ -13,6 +9,11 @@ from numpy import nan as Nan
 from numpy import inf as inf
 from tqdm import notebook as tqdm
 from scipy.sparse import csr_matrix
+from utils import load_csv, save_csv
+
+from pymatgen.core import Structure
+from ase import units
+
 
 def nan_and_inf_finder(features):
     """
@@ -36,6 +37,7 @@ def nan_and_inf_finder(features):
     lost_features_count = np.logical_or(nan_array, inf_array).sum()
     valid_features = np.logical_not(np.logical_or(nan_array, inf_array))
     return lost_features_count, valid_features
+
 
 def nan_and_inf_finder_SOAP(features):
     """
@@ -61,27 +63,249 @@ def nan_and_inf_finder_SOAP(features):
             lost_features_count = 0
             valid_features = np.ones(np.shape(features)[0], dtype=bool)
             return lost_features_count, valid_features
-        
 
 
-def save_sparse_features(features, filename):
+def make_features(path):
     """
-    Function to save a sparse feature representation for each feature. The files are saved with the same name
-    but in a new directory: 'saved_sparse_features'.
+    Function to gather features from path folder into one dataframe.
 
     Parameters
     ----------
-    features : np.array
-        A feature representation for each structure.
+    path : str
+        Path to folder with features.
         
-    filename: str
-        The original filename for the feature. 
-    """ 
-    sparse_features = csr_matrix(features)
+    Returns
+    -------
+    files : list of str
+        A list of pathes to the features. 
     
-    # save the sparse representation
-    save_path = os.path.join(os.getcwd(), 'sparse_features/{}.pkl'.format(filename))
-    save_file = open(save_path, 'wb')
-    pickle.dump(sparse_features, save_file)
-    save_file.close()
-    return sparse_features
+    feature_list : list
+        list of features
+
+    cnt_of_nan_features: list
+        list of number of nans for all features
+    """   
+    
+    files = sorted(os.listdir(path))
+
+    feature_list = []
+    cnt_of_nan_features = []
+    name_of_features = []
+
+    cnt_and_name_features = {}
+
+    for file in files:
+        if re.search('mask', file):
+            print(file)
+            continue
+        if re.search('SOAP', file):
+            features = csr_matrix(np.load(io.BytesIO(open('{}/{}'.format(path, file), 'rb').read()), allow_pickle=True).all())
+            lost_features_count, _ = nan_and_inf_finder_SOAP(features)
+        elif re.search('ipynb_checkpoints', file):
+            next
+        else:
+            features = np.load('{}/{}'.format(path, file), allow_pickle=True)
+            feature_list.append(features)
+            lost_features_count, _ = nan_and_inf_finder(features)
+
+        name_of_features.append(file.split('_mode-structure', 1)[0])
+        # print(file, name_of_features[-1], features.shape)
+
+        cnt_and_name_features[name_of_features[-1]] = features.shape[1]
+
+        # if lost_features_count != 0:
+            # print("{} rows are lost in the feature: {}".format(lost_features_count, file))
+        cnt_of_nan_features.append(lost_features_count)
+    return name_of_features, feature_list, cnt_of_nan_features, cnt_and_name_features
+
+
+
+def make_plane(labels_df, non_embedding_features):
+    """
+    From embeddings to plane features.
+
+    Parameters
+    ----------
+    labels_df : pd.DataFrame
+        Path to folder with features.
+        
+    Returns
+    -------
+    labels_df : list of str
+        A list of pathes to the features. 
+    
+    zero_columns : list
+        list of features
+    """   
+
+    features = labels_df.drop(non_embedding_features, axis = 1).columns
+    df_temp = pd.DataFrame()
+
+    for _, feature in enumerate(features):
+        data = labels_df[feature].to_numpy()
+        data = np.array([np.array(x) for x in data]).T
+        lenn = data.shape[0]
+        columns = np.arange(lenn)
+        columns = [str(x) + '_' + feature for x in columns]
+        dictionary = dict(zip(columns, data))
+        df = pd.DataFrame(dictionary)
+        df_temp = pd.concat([df_temp, df], axis = 1)
+
+    labels_df = pd.concat([df_temp, labels_df], axis = 1)
+    labels_df = labels_df.drop(features, axis = 1)
+    zero_columns = []
+
+    for column in labels_df.columns:
+        if labels_df[column].nunique() == 1:
+            zero_columns.append(column)
+    return labels_df, zero_columns
+
+
+
+def merging_features(path_to_oxi_states, path_to_feature_folder, path_to_save):
+
+    """
+    merge embeddings to dataset of plane features
+
+    Parameters
+    ----------
+    path_to_oxi_states : pd.DataFrame
+        Path to dataset with oxi_states and material names.
+        
+    path_to_feature_folder: str
+        Path to folder with features (.npy files)
+
+    path_to_save: str
+        Path to save csv dataframe
+
+    Returns
+    -------
+    labels_df : list of str
+        A list of pathes to the features. 
+    
+    nan_features : set
+        set of features with Nans
+    """   
+        
+    nan_features = set()
+    labels_df = load_csv(path_to_oxi_states)
+    files, feature_list, cnt_of_nan_features, cnt_and_name_features = make_features(path_to_feature_folder)
+    for i, feature in enumerate(feature_list):
+        # print(files[i])
+        labels_df[files[i]] = feature.tolist()
+        if cnt_of_nan_features[i] != 0:
+            nan_features.add(files[i])
+    labels_df.reset_index(drop=True, inplace=True)
+    # save_csv(labels_df, path_to_save)
+    return labels_df, nan_features, cnt_and_name_features
+
+
+
+
+def dataset_preprocessing(df, structure_column):
+    df[structure_column] = df[structure_column].apply(lambda x: Structure.from_str(x, 'json'))
+    df['n_Li'] = df[structure_column].apply(lambda x: x.composition["Li"] / x.lattice.volume)
+
+    D_to_sigma_factor = (
+        df["n_Li"]  # 1 / A^3
+            / (1000 * units.kB)  # eV
+            * (1e24 / units.C)  # (A/cm)^3 * (e / C)
+        )
+
+
+    df.loc[df['diffusion_mean_cm2_s'] <= 1e-15, 'diffusion_mean_cm2_s'] = 1e-15
+
+    df['sigma_S_cm'] = D_to_sigma_factor * df['diffusion_mean_cm2_s']
+    df['sigma_S_cm_sem'] = D_to_sigma_factor * df['diffusion_sem_cm2_s']
+    df['sigma_S_cm_err'] = df['sigma_S_cm_sem']
+
+    df.loc[df['group'] == 'group_A', 'group'] = 'A'
+    df.loc[df['group'] == 'group_B', 'group'] = 'B1'
+    df.loc[df['group'] == 'group_E', 'group'] = 'B2'
+    df.loc[df['group'] == 'group_C', 'group'] = 'C'
+
+    return df
+
+def get_featurizers_features():
+    """
+    Pull together features from matminer.featurizers to single dataset
+    ----------
+    """
+
+    path_to_oxi_states = 'groups_and_oxi_states_5_frames/df_step_0.pkl'
+    path_to_feature_folder = 'kahle_features'
+    path_to_save = 'kahle.pkl'
+    
+    df_kahle, nan_features_kahle, cnt_and_name_features_kahle = merging_features(path_to_oxi_states, path_to_feature_folder, path_to_save)
+
+    df_kahle = df_kahle.drop(['stru_traj', 'structure_A', 'structure_AM',
+       'structure_CAN', 'structure_CAMN', 'structure_A40', 'structure_AM40',
+       'structure_CAN40', 'structure_CAMN40'], axis = 1)
+
+    df_kahle_plane, zero_columns_kahle = make_plane(df_kahle, ['stru_label', 'stru_id', 'group'])
+    df_kahle_plane = df_kahle_plane.drop(zero_columns_kahle, axis = 1)
+
+    for col in df_kahle_plane[df_kahle_plane.columns[df_kahle_plane.isna().any()].tolist()].isna().sum().to_frame().T.columns:
+        df_kahle_plane[col].fillna((df_kahle_plane[col].mean()), inplace=True)
+
+    df_kahle_plane = df_kahle_plane[df_kahle_plane['group'] != 'group_D']
+    kahle = pd.read_csv('kahle.csv')
+    df_kahle_fin = df_kahle_plane.merge(kahle, left_on = ['stru_label', 'stru_id', 'group'], right_on = ['stru_label', 'stru_id', 'group'])
+
+
+    df_kahle_fin = dataset_preprocessing(df_kahle_fin, 'structure') 
+
+    return df_kahle_fin, cnt_and_name_features_kahle
+
+
+
+
+def get_featurizers_features_mpdb():
+    """
+    Pull together features from matminer.featurizers to single dataset
+    ----------
+    """
+
+    path_to_oxi_states = 'mpdb/mpdb.pkl'
+    path_to_feature_folder = 'mpdb_features'
+    path_to_save = 'mpdb/structure_with_features.pkl'
+    df_mpdb, nan_features_mpdb, cnt_and_name_features_mpdb = merging_features(path_to_oxi_states, path_to_feature_folder, path_to_save)
+
+    df_mpdb = df_mpdb.drop(['nsites', 'elements', 'nelements',
+        'volume', 'energy_above_hull', 'band_gap', 'database_IDs', 'structure_A', 'structure_AM', 'structure_CAN', 'structure',
+        'structure_CAMN', 'structure_A40', 'structure_AM40', 'structure_CAN40', 'structure_CAMN40', 'fields_not_requested'], axis = 1)
+
+    df_mpdb_plane, zero_columns_mpdb = make_plane(df_mpdb, ['formula_pretty', 'material_id'])
+
+
+
+    path_to_oxi_states = 'groups_and_oxi_states_5_frames/df_step_0.pkl'
+    path_to_feature_folder = 'kahle_features'
+    path_to_save = 'kahle.pkl'
+    
+    df_kahle, nan_features_kahle, cnt_and_name_features_kahle = merging_features(path_to_oxi_states, path_to_feature_folder, path_to_save)
+    df_kahle = df_kahle.drop(['stru_traj', 'structure_A', 'structure_AM',
+        'structure_CAN', 'structure_CAMN', 'structure_A40', 'structure_AM40',
+        'structure_CAN40', 'structure_CAMN40'], axis = 1)
+
+    df_kahle_plane, zero_columns_kahle = make_plane(df_kahle, ['stru_label', 'stru_id', 'group'])
+
+
+    df_mpdb_plane = df_mpdb_plane.drop(zero_columns_kahle, axis = 1)
+    for col in df_mpdb_plane[df_mpdb_plane.columns[df_mpdb_plane.isna().any()].tolist()].isna().sum().to_frame().T.columns:
+        df_mpdb_plane[col].fillna((df_mpdb_plane[col].mean()), inplace=True)
+
+    return df_mpdb_plane, cnt_and_name_features_mpdb
+
+
+def get_nn_features_kahle():
+    df_barrier_features_kahle = pd.read_csv('fv.v2.Kahle2020-upd2024-03-04/exported.predictions.Kahle2020.v2.csv')
+
+    barrier_robust_0p_features = list(filter(lambda x: x.find('barrier_robust_0p') != -1 and x.find('masked1p5') == -1, df_barrier_features_kahle.columns.to_list()))
+    union_features = list(filter(lambda x: x.find('union') != -1 and x.find('masked1p5') == -1, df_barrier_features_kahle.columns.to_list()))
+    df_barrier_features_kahle = df_barrier_features_kahle[barrier_robust_0p_features + union_features + ['src_id', 'diffusion_mean_cm2_s']]
+
+    kahle = pd.read_csv('kahle.csv')
+    df_kahle_fin = df_barrier_features_kahle.merge(kahle, left_on = ['src_id', 'diffusion_mean_cm2_s'], right_on = ['src_id', 'diffusion_mean_cm2_s'])
+    df_kahle_fin = dataset_preprocessing(df_kahle_fin, 'structure')
+
